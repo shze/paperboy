b94a2c942b1a7a802b3716335708cce4e457daea
Sten Heinze <sten.heinze@gmail.com>
2023-07-27 17:47:14 -0400

    Move update to thread to not block gui.
    * Use QThread to run update in background.
    * Update update menu text according to state.

--- src/paperboy-gui.py
+++ src/paperboy-gui.py
@@ -2,10 +2,35 @@
 
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QTimer, QTime
+from PyQt5.QtCore import QTimer, QTime, QThread
 from notifypy import Notify
 import paperboy as pb
 import logging
+import time
+
+
+MENU_UPDATE_TEXT_IDLE = "Check for new articles"
+MENU_UPDATE_TEXT_UPDATING = "Checking for new articles.."
+
+
+class UpdateWorker(QThread):
+    def run(self): 
+        p = pb.Paperboy()
+        p.update_article_ids()
+
+        notification = Notify(
+            default_notification_title="Function Message",
+            #default_application_name="Great Application",
+            default_notification_application_name="not a python app",
+            default_notification_icon="/usr/share/icons/hicolor/32x32/status/laptoptrusted.svg",
+            #default_notification_audio="path/to/sound.wav"
+        )
+        notification.message = "Even cooler message."
+        notification.send()
+
+        time.sleep(10)
+
+        self.finished.emit()
 
 
 class App:
@@ -18,7 +43,6 @@ class App:
         self.app = QApplication([])
         self.app.setQuitOnLastWindowClosed(False)
 
-        # TODO better icon, depending on DE and state
         icon = QIcon.fromTheme("applications-science")
         self.tray = QSystemTrayIcon()
         self.tray.setIcon(icon)
@@ -32,7 +56,7 @@ class App:
         self.m_show_articles.setIcon(QIcon.fromTheme('system-software-install'))
         self.menu.addAction(self.m_show_articles)
 
-        self.m_update_articles = QAction("Check for new articles")
+        self.m_update_articles = QAction(MENU_UPDATE_TEXT_IDLE)
         self.m_update_articles.triggered.connect(self.f_update_articles)
         self.m_update_articles.setIcon(QIcon.fromTheme('view-refresh'))
         self.menu.addAction(self.m_update_articles)
@@ -48,36 +72,26 @@ class App:
         self.timer.timeout.connect(self.f_update_articles)
         self.timer.start(1000)
 
-        self.p = pb.Paperboy()
-
     def run(self):
         self.app.exec_()
 
     def f_update_running(self, is_running):
         if is_running:
             self.timer.stop()
+            self.m_update_articles.setText(MENU_UPDATE_TEXT_UPDATING)
             self.m_update_articles.setEnabled(False)
         else:
             self.timer.start(600000) # 10min
+            self.m_update_articles.setText(MENU_UPDATE_TEXT_IDLE)
             self.m_update_articles.setEnabled(True)
 
     def f_update_articles(self):
         self.f_update_running(True)
-    
-        self.p.update_article_ids()
 
-        notification = Notify(
-            default_notification_title="Function Message",
-            #default_application_name="Great Application",
-            default_notification_application_name="not a python app",
-            default_notification_icon="/usr/share/icons/hicolor/32x32/status/laptoptrusted.svg",
-            #default_notification_audio="path/to/sound.wav"
-        )
-        notification.message = "Even cooler message."
-        notification.send()
+        self.w = UpdateWorker()
+        self.w.finished.connect(lambda: self.f_update_running(False))
+        self.w.start()
 
-        self.f_update_running(False)
-    
 
 if __name__ == '__main__':
     App().run()
